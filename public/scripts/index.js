ec7c882e574659879ee7dfd096ceb1fd4e2ad1c0
John Vitor <jobitencourt924@gmail.com>
2022-02-27 18:34:41 -0300

    popup modal done

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -9,3 +9,26 @@ document.addEventListener("DOMContentLoaded", (e) =>{
         document.getElementById("pageContent").style.removeProperty("display");
     }, 1500);
 });
+
+/* poop up */
+
+const userArea = document.getElementById("userArea");
+const popup = document.querySelector(".modalConteiner");
+const popupBlur = document.querySelector(".pageContent")
+userArea.addEventListener("click", () =>{
+    popupBlur.classList.add("blur");
+    popup.style.display = "flex";
+});
+
+popup.addEventListener("click", event =>{
+    const calssNameOfClickedElement = event.target.classList[0];
+    const classNames = ["modalConteiner", "linkButton"];
+    const shouldClousePopup = classNames.some(classNames =>
+    classNames === calssNameOfClickedElement);
+
+    if(shouldClousePopup){
+    popup.style.display = "none";
+    popupBlur.classList.remove("blur");
+
+    }
+});
